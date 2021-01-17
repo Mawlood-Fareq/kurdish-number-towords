4dc21dc97844db00ecf9897e7109ab28048b6a4a
Alaa <alaa@24group.net>
2021-01-17 22:59:43 +0300

    Version 1.0.1
    Convert Currency

--- README.md
+++ README.md
@@ -11,13 +11,40 @@ npm i kurdish-number-towords --save
 ## Usage
 ### Importing
 ```js
-const { convert } = require('kurdish-number-towords');
+const { convert, convertCurrency } = require('kurdish-number-towords');
 ```
 OR
 ```js
-import { convert } from 'kurdish-number-towords';
+import { convert, convertCurrency } from 'kurdish-number-towords';
 ```
+
 ### To convert Number to words
 ```js
 console.log(convert(123)) // سەد و بیست و سێ
 ```
+
+### To convert Currency to words
+```js
+console.log(convertCurrency(123)) // سەد و بیست و سێ دینار
+```
+Use Currency with including decimal points :
+```js
+console.log(convertCurrency(123.25)) // سەد و بیست و سێ دینار و بیست و پێنج فلس
+```
+
+_Note. By default Currency unit is __Dinar__ and decimal unit is __Flis__, you can change to any Currency and decimal unit :_
+```js
+console.log(convertCurrency(123, 'دۆلار')) // سەد و بیست و سێ دۆلار
+
+console.log(convertCurrency(123.25, 'دۆلار', 'سەنت')) // سەد و بیست و سێ دۆلار و بیست و پێنج سەنت
+```
+
+
+## Change Log
+
+__Version 1.0.1__
+* Converts Currency into words.
+* Use Special Currency and decimal units.
+
+__Version 1.0.0__
+* Converts Numbers into words for Central Kurdish Language.
--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-
-module.exports.convert = function convert(number, level=0) {
+// Convert Number
+module.exports.convert = function convert(number, level = 0) {
   var ones = ["یەک", "دوو", "سێ", "چوار", "پێنج", "شەش", "حەوت", "هەشت", "نۆ"];
-  var ten = ["ده", "یازده", "دوازدە", "سیازده", "چوارده", "پازده", "شازده", "حەڤده", "هەژده", "نۆزده"];
+  var ten = ["دە", "یازدە", "دوازدە", "سیازدە", "چواردە", "پازدە", "شازدە", "حەڤدە", "هەژدە", "نۆزدە"];
   var tens = ["بیست", "سی", "چل", "پەنجا", "شەست", "حەفتا", "هەشتا", "نەوەد"];
   var hundreds = ["سەد", "دوو سەد", "سێ سەد", "چوار سەد", "پێنج سەد", "شەش سەد", "حەوت سەد", "هەشت سەد", "نۆ سەد"];
-  // thousands
 
   let result = "";
 
   if (number === null)
-  return "";
+    return "";
 
   if (number < 0) {
     number = number * -1;
     return "سالب " + convert(number, level);
   }
 
-  if (number === 0) {
+  if (number == 0) {
     if (level === 0) {
       return "سفر";
     } else {
@@ -46,7 +45,66 @@ module.exports.convert = function convert(number, level=0) {
   } else if (number < 1000000000000000) {
     result += convert(Math.floor(number / 1000000000000), level) + " تریلیۆن" + convert(number % 1000000000000, level + 1);
   }
-
   return result;
+}
 
+// Convert Currency
+module.exports.convertCurrency = function convertCurrency(number, currencyUnit = "دینار", decimalUnit = "فلس") {
+
+  function convert(number, level = 0) {
+    var ones = ["یەک", "دوو", "سێ", "چوار", "پێنج", "شەش", "حەوت", "هەشت", "نۆ"];
+    var ten = ["دە", "یازدە", "دوازدە", "سیازدە", "چواردە", "پازدە", "شازدە", "حەڤدە", "هەژدە", "نۆزدە"];
+    var tens = ["بیست", "سی", "چل", "پەنجا", "شەست", "حەفتا", "هەشتا", "نەوەد"];
+    var hundreds = ["سەد", "دوو سەد", "سێ سەد", "چوار سەد", "پێنج سەد", "شەش سەد", "حەوت سەد", "هەشت سەد", "نۆ سەد"];
+
+    let result = "";
+
+    if (number === null)
+      return "";
+
+    if (number < 0) {
+      number = number * -1;
+      return "سالب " + convert(number, level);
+    }
+
+    if (number == 0) {
+      if (level === 0) {
+        return "سفر";
+      } else {
+        return "";
+      }
+    }
+
+    if (level > 0) {
+      result += " و ";
+      level -= 1;
+    }
+
+    if (number < 10) {
+      result += ones[number - 1];
+    } else if (number < 20) {
+      result += ten[number - 10];
+    } else if (number < 100) {
+      result += tens[Math.floor(number / 10) - 2] + convert(number % 10, level + 1);
+    } else if (number < 1000) {
+      result += hundreds[Math.floor(number / 100) - 1] + convert(number % 100, level + 1);
+    } else if (number < 1000000) {
+      result += (number / 1000 < 2 ? '' : convert(Math.floor(number / 1000), level)) + " هەزار" + convert(number % 1000, level + 1);
+    } else if (number < 1000000000) {
+      result += convert(Math.floor(number / 1000000), level) + " ملیۆن" + convert(number % 1000000, level + 1);
+    } else if (number < 1000000000000) {
+      result += convert(Math.floor(number / 1000000000), level) + " میلیارد" + convert(number % 1000000000, level + 1);
+    } else if (number < 1000000000000000) {
+      result += convert(Math.floor(number / 1000000000000), level) + " تریلیۆن" + convert(number % 1000000000000, level + 1);
+    }
+    return result;
+  }
+
+  var _split = number.toString().split(".")
+
+  if (_split.length > 1) {
+    return `${convert(_split[0])} ${currencyUnit} و ${convert(_split[1])} ${decimalUnit}`
+  } else {
+    return `${convert(_split[0])} ${currencyUnit}`
+  }
 }
